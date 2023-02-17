c4f918b59c629f9e9b78108160a1ed5e8106f92d
Michael Ge <michaelge@wustl.edu>
2023-02-17 11:45:20 -0600

    beginning number guess game

--- tutorial/numberGuess/app.js
+++ tutorial/numberGuess/app.js
@@ -1,1 +1,23 @@
-
+Vue.createApp({
+  data() {
+    return {
+      input: 0,
+      turnCounter: 0,
+      priorGuesses: [],
+      numberToGuess: 0,
+    };
+  },
+  methods: {
+    submit() {
+      this.priorGuesses.push(this.input);
+      checkCloseness(this.input);
+      this.input = 0;
+    },
+    checkCloseness(userInput) {},
+  },
+  computed: {
+    generateRandomNumber() {
+      return (this.numberToGuess = Math.floor(Math.random() * 100) + 1);
+    },
+  },
+}).mount("#app");
