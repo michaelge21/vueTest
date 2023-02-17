2b8ee89175508ca901757923a757b7f1a72a9564
Michael Ge <michaelge@wustl.edu>
2023-02-17 15:33:11 -0600

    MVP OF THE GAME!!!

--- tutorial/numberGuess/app.js
+++ tutorial/numberGuess/app.js
@@ -1,23 +1,56 @@
 Vue.createApp({
   data() {
     return {
-      input: 0,
+      input: 1,
       turnCounter: 0,
       priorGuesses: [],
-      numberToGuess: 0,
+      numberToGuess: this.generateRandomNumber(),
+      status: "",
     };
   },
   methods: {
     submit() {
-      this.priorGuesses.push(this.input);
-      checkCloseness(this.input);
-      this.input = 0;
+      this.turnCounter++;
+      this.feedback();
     },
-    checkCloseness(userInput) {},
-  },
-  computed: {
+
+    feedback() {
+      if (this.input == this.numberToGuess) {
+        this.status = "victory";
+      }
+
+      if (this.input == this.numberToGuess && this.turnCounter > 7) {
+        this.status = "halfVictory";
+      }
+
+      if (
+        (this.turnCounter == 7 && this.input > this.numberToGuess) ||
+        (this.turnCounter == 7 && this.input < this.numberToGuess)
+      ) {
+        this.status = "gameOver";
+      }
+
+      if (this.input > this.numberToGuess) {
+        this.priorGuesses.push(this.input + " is too HIGH");
+      } else if (this.input < this.numberToGuess) {
+        this.priorGuesses.push(this.input + " is too LOW");
+      }
+    },
+
+    gameReset() {
+      this.turnCounter = 0;
+      this.priorGuesses.length = 0;
+      this.status = false;
+      this.generateRandomNumber();
+    },
+
     generateRandomNumber() {
       return (this.numberToGuess = Math.floor(Math.random() * 100) + 1);
     },
+  },
+  computed: {
+    // autoGen() {
+    //   return (this.numberToGuess = Math.floor(Math.random() * 100) + 1);
+    // },
   },
 }).mount("#app");
