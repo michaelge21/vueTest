3e036b38fd9548df2419c1aa99b82905abd20d5c
Michael Ge <michaelge@wustl.edu>
2023-02-21 12:14:03 -0600

    testing hot reloading

--- use-vite/first-vite-project/main.js
+++ use-vite/first-vite-project/main.js
@@ -10,7 +10,8 @@ document.querySelector("#app").innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Vite!!</h1>
+    <p>I'm testing out the hot reloading feature of Vite, a module bundler built by the creator of Vue.</p>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
