f8c00b76e5124105052680a0cc91ed87bad290b9
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2023-02-21 19:10:23 -0600

    saving blog post component

---
+++ use-vite/vue-init/components/src/components/BlogPost.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>{{ desc }}</p>
+</template>
+
+<script>
+export default {
+  name: "BlogPost",
+  props: ["title", "desc"],
+};
+</script>
--- use-vite/vue-init/components/src/components/Main.vue
+++ use-vite/vue-init/components/src/components/Main.vue
@@ -2,18 +2,66 @@
   <main>
     <h1>this is some main content</h1>
     <p>this will only grow as large as the content within</p>
+    <ul>
+      <li>
+        <BlogPost title="testing1" desc="testing"></BlogPost>
+      </li>
+      <li>
+        <BlogPost title="sample title" desc="wow, clickbait!"></BlogPost>
+      </li>
+      <li>
+        <BlogPost title="another sample" desc="no clickbait >:("></BlogPost>
+      </li>
+
+      <!-- <li v-for="post in BlogPosts">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.desc }}</p>
+      </li> -->
+    </ul>
   </main>
 </template>
 
 <script>
+import BlogPost from "@/components/BlogPost.vue";
 export default {
   name: "Main",
+  data() {
+    return {
+      //   BlogPosts: [
+      //     {
+      //       title: "test",
+      //       desc: "testingDesc",
+      //     },
+      //     {},
+      //     {},
+      //   ],
+    };
+  },
+  components: {
+    BlogPost,
+  },
 };
 </script>
 
 <style scoped>
 main {
   background-color: grey;
-  text-align: center;
+}
+
+h1 {
+  color: pink;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: row wrap;
+
+  list-style: none;
+}
+
+li {
+  flex: 1 400px;
 }
 </style>
